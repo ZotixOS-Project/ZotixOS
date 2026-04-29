2b87c8ff5a90c5336f20fcf91149d0609d267b93
Artem <artemkolba321@gmail.com>
2026-04-29 12:08:52 +0700

    Fix: initramfs

--- airootfs/usr/local/bin/zotix-mirrors.sh
+++ airootfs/usr/local/bin/zotix-mirrors.sh
@@ -2,12 +2,20 @@
 set -e
 
 echo "[ZotixOS] Running automatic reflector (no country bias)..."
-
-reflector \
-  --latest 30 \
-  --protocol https \
-  --sort rate \
-  --number 15 \
-  --save /etc/pacman.d/mirrorlist
-
+if [ -n "$COUNTRY" ]; then
+    reflector \
+        --country "$COUNTRY" \
+        --latest 30 \
+        --protocol https \
+        --sort rate \
+        --number 15 \
+        --save /etc/pacman.d/mirrorlist
+else
+    reflector \
+        --latest 30 \
+        --protocol https \
+        --sort rate \
+        --number 15 \
+        --save /etc/pacman.d/mirrorlist
+fi
 echo "[ZotixOS] Mirrorlist generated."
