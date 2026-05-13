6083885886e5deea4ac514e42093f5b44cbf6411
Artem <artemkolba321@gmail.com>
2026-05-13 13:15:52 +0700

    Fix: ISO

--- profiledef.sh
+++ profiledef.sh
@@ -3,7 +3,7 @@
 
 
 iso_name="ZotixOS-linux"
-iso_version="$(date +%Y.%m.%d)"
+iso_version="v0.1.0"
 iso_label="ZOTIXOS"
 iso_application="ZotixOS Live/Main System"
 install_dir="arch"
@@ -12,8 +12,10 @@ bootmodes=('uefi.grub')
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=(
-  '-comp xz'
-  '-b 1M'
+  '-comp'
+  'xz'
+  '-b'
+  '1M'
 )
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 
